eabed4aeb60b872bbd45321f337cac704ed052d3
Eldorado Agalliu <eldoradoagalliu@gmail.com>
2025-02-08 23:33:33 +0100

    Update web security config

--- src/main/java/com/limitlessmoviesapp/config/WebSecurityConfig.java
+++ src/main/java/com/limitlessmoviesapp/config/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package com.limitlessmoviesapp.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
-    private UserDetailsService userDetailsService;
+
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -20,26 +24,24 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                    .antMatchers("/resources/**", "/static/**","/webjars/**", "/css/**", "/js/**", "/images/**","/register", "/login").permitAll()
-                    .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("email") // For login will be required email not a username
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/WEB-INF/**", "/webjars/**", "/css/**", "/images/**",
+                                        "/login", "/register").permitAll()
+                                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                                .anyRequest().authenticated())
+                .formLogin(form ->
+                        form.loginPage("/login")
+                                .usernameParameter("email") // For login will be required email not a username
+                                .permitAll())
+                .logout(logout ->
+                        logout.logoutUrl("/logout")
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .authenticationProvider(authenticationProvider);
 
         return http.build();
-    }
-
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
